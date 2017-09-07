edec1891e1eb486f7c3b5ac39b4b0db6e649a75f
nbecker <nils.becker@bioquant>
2017-09-07 14:51:38 +0200

    corrected remaining old ounit calls
    now complies with ounit 2.0

--- test/evidence_test.ml
+++ test/evidence_test.ml
@@ -1,83 +1,83 @@
 open OUnit
 
 module Ev = Evidence.Make(
-  struct 
+  struct
     type params = float array
     let to_coords (p : params) = p
   end)
 
 let pi = 4.0 *. (atan 1.0)
 
-let random_between a b = 
+let random_between a b =
   a +. (Random.float (b-.a))
 
-let log_multi_gaussian mu sigma x = 
-  let n = Array.length x in 
-  let res = ref 0.0 in 
-    for i = 0 to n - 1 do 
-      let dx = mu.(i) -. x.(i) in 
+let log_multi_gaussian mu sigma x =
+  let n = Array.length x in
+  let res = ref 0.0 in
+    for i = 0 to n - 1 do
+      let dx = mu.(i) -. x.(i) in
         res := !res -. 0.5*.(log (2.0*.pi));
         res := !res -. (log sigma.(i));
         res := !res -. dx*.dx/.(2.0*.sigma.(i)*.sigma.(i))
     done;
     !res +. 0.0
 
-let log_uniform_prior x = 
-  let i = ref 0 and 
+let log_uniform_prior x =
+  let i = ref 0 and
       outside = ref false and
-      n = Array.length x in 
+      n = Array.length x in
     while (!i < n && not !outside) do
-      if x.(!i) < 0.0 || x.(!i) > 1.0 then 
+      if x.(!i) < 0.0 || x.(!i) > 1.0 then
         outside := true;
       incr i
     done;
-    if !outside then 
+    if !outside then
       neg_infinity
     else
       0.0
 
-let multi_gaussian_propose sigma x = 
-  let n = Array.length x in 
-  let ndim = float_of_int n in 
-  let xnew = Array.make n 0.0 in 
-    for i = 0 to n - 1 do 
-      let dx = sigma.(i) /. ndim in 
+let multi_gaussian_propose sigma x =
+  let n = Array.length x in
+  let ndim = float_of_int n in
+  let xnew = Array.make n 0.0 in
+    for i = 0 to n - 1 do
+      let dx = sigma.(i) /. ndim in
         xnew.(i) <- x.(i) +. (random_between (~-.dx) dx)
     done;
     xnew
 
-let assert_equal_float ?(epsabs = 1e-8) ?(epsrel = 1e-8) = 
-  assert_equal ~printer:string_of_float ~cmp:(cmp_float ~epsabs:epsabs ~epsrel:epsrel)
+let assert_equal_float ?(epsabs = 1e-8) ?(epsrel = 1e-8) =
+  assert_equal ~printer:string_of_float ~cmp:(cmp_float ~epsilon:epsabs)
 
-let test_evidence_direct_2d () = 
-  let mu = Array.init 2 (fun _ -> Random.float 1.0) and 
-      sigma = Array.init 2 (fun _ -> Random.float 1.0 +. 1.0) in 
-  let all_samples = 
-    (Mcmc.mcmc_array 10000 (log_multi_gaussian mu sigma) (fun _ -> 0.0) 
-       (multi_gaussian_propose sigma) (fun _ _ -> 0.0) mu) in 
-  let samples = Mcmc.remove_repeat_samples (=) all_samples in 
-  let ev = Ev.evidence_direct ~n:64 samples in 
+let test_evidence_direct_2d () =
+  let mu = Array.init 2 (fun _ -> Random.float 1.0) and
+      sigma = Array.init 2 (fun _ -> Random.float 1.0 +. 1.0) in
+  let all_samples =
+    (Mcmc.mcmc_array 10000 (log_multi_gaussian mu sigma) (fun _ -> 0.0)
+       (multi_gaussian_propose sigma) (fun _ _ -> 0.0) mu) in
+  let samples = Mcmc.remove_repeat_samples (=) all_samples in
+  let ev = Ev.evidence_direct ~n:64 samples in
     assert_equal_float ~epsabs:0.5 ~msg:"evidence not 1 for gaussian posterior" 1.0 ev
 
-let test_evidence_harmonic_mean_1d () = 
-  let mu = Random.float 1.0 and 
-      sigma = Random.float 1.0 in 
-  let log_like x = Stats.log_gaussian mu sigma x.(0) and 
-      log_prior _ = ~-.(log 20.0) in 
-  let propose x = [|Mcmc.uniform_wrapping (-10.0) 10.0 sigma x.(0)|] and 
-      log_jp _ _ = 0.0 in 
-  let samples = 
-    Mcmc.mcmc_array 1000000 log_like log_prior propose log_jp [|mu|] in 
-  let ev = Ev.evidence_harmonic_mean samples in 
+let test_evidence_harmonic_mean_1d () =
+  let mu = Random.float 1.0 and
+      sigma = Random.float 1.0 in
+  let log_like x = Stats.log_gaussian mu sigma x.(0) and
+      log_prior _ = ~-.(log 20.0) in
+  let propose x = [|Mcmc.uniform_wrapping (-10.0) 10.0 sigma x.(0)|] and
+      log_jp _ _ = 0.0 in
+  let samples =
+    Mcmc.mcmc_array 1000000 log_like log_prior propose log_jp [|mu|] in
+  let ev = Ev.evidence_harmonic_mean samples in
     assert_equal_float ~epsabs:0.9 ~msg:"evidence not 1 for gaussian posterior" 1.0 ev
 
-let test_evidence_lebesgue_2d () = 
-  let mu = Array.init 2 (fun _ -> Random.float 1.0) and 
-      sigma = Array.init 2 (fun _ -> Random.float 0.1) in 
-  let all_samples = 
-    (Mcmc.mcmc_array 10000 (log_multi_gaussian mu sigma) log_uniform_prior 
-       (multi_gaussian_propose sigma) (fun _ _ -> 0.0) mu) in 
-  let ev = Ev.evidence_lebesgue ~eps:0.2 ~n:64 all_samples in 
+let test_evidence_lebesgue_2d () =
+  let mu = Array.init 2 (fun _ -> Random.float 1.0) and
+      sigma = Array.init 2 (fun _ -> Random.float 0.1) in
+  let all_samples =
+    (Mcmc.mcmc_array 10000 (log_multi_gaussian mu sigma) log_uniform_prior
+       (multi_gaussian_propose sigma) (fun _ _ -> 0.0) mu) in
+  let ev = Ev.evidence_lebesgue ~eps:0.2 ~n:64 all_samples in
     assert_equal_float ~epsabs:0.5 ~msg:"evidence not 1 for gaussian posterior" 1.0 ev
 
 let tests = "evidence.ml tests" >:::
--- test/read_write_test.ml
+++ test/read_write_test.ml
@@ -1,77 +1,77 @@
 open OUnit
 open Asserts
 
-let mcmc_samples_close a b = 
-  let n = Array.length a in 
+let mcmc_samples_close a b =
+  let n = Array.length a in
     assert_equal ~printer:string_of_int ~msg:"lengths differ" n (Array.length b);
-    for i = 0 to n - 1 do 
+    for i = 0 to n - 1 do
       let {Mcmc.value = va;
            like_prior = {Mcmc.log_likelihood = lla;
-                         log_prior = lpa}} = a.(i) and 
+                         log_prior = lpa}} = a.(i) and
           {Mcmc.value = vb;
            like_prior = {Mcmc.log_likelihood = llb;
-                         log_prior = lpb}} = b.(i) in 
-        assert_equal ~printer:string_of_float ~msg:"values differ" 
-          ~cmp:(cmp_float ~epsabs:1e-3 ~epsrel:1e-3) va vb;
-        assert_equal ~printer:string_of_float ~msg:"log_likelihoods differ" 
-          ~cmp:(cmp_float ~epsabs:1e-3 ~epsrel:1e-3) lla llb;
-        assert_equal ~printer:string_of_float ~msg:"values differ" 
-          ~cmp:(cmp_float ~epsabs:1e-3 ~epsrel:1e-3) lpa lpb
+                         log_prior = lpb}} = b.(i) in
+        assert_equal ~printer:string_of_float ~msg:"values differ"
+          ~cmp:(cmp_float ~epsilon:1e-3) va vb;
+        assert_equal ~printer:string_of_float ~msg:"log_likelihoods differ"
+          ~cmp:(cmp_float ~epsilon:1e-3) lla llb;
+        assert_equal ~printer:string_of_float ~msg:"values differ"
+          ~cmp:(cmp_float ~epsilon:1e-3) lpa lpb
     done
 
-let test_read_write_inverses () = 
-  let samples = 
+let test_read_write_inverses () =
+  let samples =
     Mcmc.mcmc_array 1000
       (fun x -> Stats.log_gaussian 0.0 1.0 x)
       (fun _ -> 0.0)
       (fun x -> x +. Random.float 1.0 -. 0.5)
       (fun x y -> 0.0)
       0.0 in
-  let (fname, ofile) = Filename.open_temp_file "mcmc_test" ".dat" in 
-    (try 
+  let (fname, ofile) = Filename.open_temp_file "mcmc_test" ".dat" in
+    (try
        Read_write.write (fun x -> [| x |]) ofile samples
-     with 
+     with
        | x -> close_out ofile; Sys.remove fname; raise x);
     close_out ofile;
-    let ifile = open_in fname in 
-    let rsamples = 
+    let ifile = open_in fname in
+    let rsamples =
       try
         Read_write.read (fun x -> x.(0)) ifile
-      with 
-        | x -> close_in ifile; Sys.remove fname; raise x in 
+      with
+        | x -> close_in ifile; Sys.remove fname; raise x in
       close_in ifile;
       Sys.remove fname;
       mcmc_samples_close samples rsamples
-    
-let test_nested_read_write () = 
-  let (log_ev, log_dev, arr_samples, log_wts) as result = 
-    Nested.nested_evidence 
+
+let test_nested_read_write () =
+  let (log_ev, log_dev, arr_samples, log_wts) as result =
+    Nested.nested_evidence
       (fun x -> x)
       (fun x -> x)
       (fun () -> [|Random.float 1.0|])
       (fun x -> Stats.log_gaussian 0.5 0.1 x.(0))
-      (fun x -> 0.0) in 
-  let samples = Array.map (fun x -> {x with Mcmc.value = x.Mcmc.value.(0)}) arr_samples in 
-  let (fname, ofile) = Filename.open_temp_file "nested_test" ".dat" in 
+      (fun x -> 0.0) in
+  let samples = Array.map (fun x -> {x with Mcmc.value = x.Mcmc.value.(0)}) arr_samples in
+  let (fname, ofile) = Filename.open_temp_file "nested_test" ".dat" in
     (try
        Read_write.write_nested (fun x -> x) ofile result
-     with 
+     with
        | x -> close_out ofile; Sys.remove fname; raise x);
     close_out ofile;
-    let ifile = open_in fname in 
-    let (rlog_ev, rlog_dev, rarr_samples, rlog_wts) = 
-      try 
+    let ifile = open_in fname in
+    let (rlog_ev, rlog_dev, rarr_samples, rlog_wts) =
+      try
         Read_write.read_nested (fun x -> x) ifile
-      with 
-        | x -> close_in ifile; Sys.remove fname; raise x in 
-    let rsamples = Array.map (fun x -> {x with Mcmc.value = x.Mcmc.value.(0)}) rarr_samples in 
+      with
+        | x -> close_in ifile; Sys.remove fname; raise x in
+    let rsamples = Array.map (fun x -> {x with Mcmc.value = x.Mcmc.value.(0)}) rarr_samples in
       close_in ifile;
       Sys.remove fname;
       assert_equal_float ~epsrel:0.01 ~msg:"evidence disagrees" log_ev rlog_ev;
       assert_equal_float ~epsrel:0.01 ~msg:"delta evidence disagrees" log_dev rlog_dev;
       mcmc_samples_close samples rsamples;
       assert_equal_float_array ~epsrel:0.01 ~msg:"weights disagree" log_wts rlog_wts
-    
+
 let tests = "read_write.ml tests" >:::
   ["read and write are inverses" >:: test_read_write_inverses;
    "read and write nested samples" >:: test_nested_read_write]
